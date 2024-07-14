3ef926958c912d8da805025435d80683798e4449
Fedor Matorin <f.matorin@mail.ru>
2024-07-14 16:39:54 +0300

    Улучшение кастомизации экспериментов

--- clean_encoder.py
+++ clean_encoder.py
@@ -10,6 +10,8 @@ from android_app_data_source import load_data, Encryption, Application
 # Настраиваемые параметры
 target_feature_name = "app_id"
 add_noise = False
+use_encoder = False
+use_ensemble = False
 
 # Загрузка данных
 data = load_data(encryption=Encryption.YES, exclude_application=[Application.MI_RU])
@@ -27,7 +29,7 @@ scale_train = pd.DataFrame(scaler.fit_transform(X_train), index=X_train.index, c
 
 scale_train[target_feature_name] = train[target_feature_name]
 
-models = AeEnsembleClassifier(encoder=False, ensemble=False, encoder_epochs=100)
+models = AeEnsembleClassifier(encoder=use_encoder, ensemble=use_ensemble, encoder_epochs=100)
 models.fit(scale_train, "app_id")
 
 if add_noise:
